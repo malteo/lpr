9c0b00f44af4dc25cc45ed65196b5fc025d11f82
Matteo Giordano <m.giordano3@studenti.unipi.it>
2009-10-05 17:09:39 +0200

    tolto un import inutile

--- src/Esercizio4/Pi.java
+++ src/Esercizio4/Pi.java
@@ -12,7 +12,6 @@
  */
 package Esercizio4;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 import java.util.logging.Level;
